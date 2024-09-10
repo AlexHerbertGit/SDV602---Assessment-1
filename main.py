e3a827be0a62436579555f6940d6fc05a5d56c4e
Alex Herbert <Alex-Herbert@live.nmit.ac.nz>
2024-09-11 10:13:08 +1200

    Created placeholder functions for fighting, added game play and simple gui window creation functions, also created the main() function for the core game logic. everything is subject to change at this stage just setting up based on templated example.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import PySimpleGUI as sg
 
 
 """
-Initialize game state and other components
+Initialize game state at starting point and other components
 """
 game_state = 'Village of Arion'
 
@@ -36,3 +36,71 @@ game_places = {
         'South': 'Whispering Forest Part 2', 'Image': 'whispering_forest_image_3'
     },
 }
+
+def initiate_fight():
+    '''Placeholder for fight module logic'''
+    return "A fight has begun with the enemy! (Fight module placeholder)"
+
+def show_current_place():
+    '''Displays the current location in game_state'''
+    return game_places[game_state]['Story']
+
+def game_play(action):
+    '''Updates the game state based on player action'''
+    global game_state
+
+    if action == 'North' or action == 'South':
+        next_place = game_places[game_state].get(action, '')
+        if next_place:
+            game_state = next_place
+            return f'Moving {action}...\n' + show_current_place()
+        else:
+            return f'You cant go {action} from here.'
+    elif action == 'Fight' and 'Fight' in game_places[game_state]:
+        '''Trigger Fight via the key in the dictionary'''
+        return initiate_fight()
+    return 'Invalid action.'
+
+def make_game_window():
+    '''Creates the PySimplueGUI game window'''
+    sg.theme('Dark Blue 3')
+
+    '''Get the image from the game_state location'''
+    current_image = game_places[game_state]['Image']
+
+    '''Layout for game window'''
+    layout = [
+        [sg.Image(current_image, size=(100, 100), key='-IMG-')],
+        [sg.Text(show_current_place(), size=(40, 5), key='-OUTPUT-')],
+        [sg.Input(key='-IN-', size=(20, 1))],
+        [sg.Button('Submit'), sg.Button('Exit')]
+    ]
+
+    return sg.Window('Legend of the Sacred Forest', layout)
+
+def main():
+    '''Main game loop logic and window creation'''
+    window = make_game_window()
+
+    while True:
+        event, values = window.read()
+        if event in (sg.WIN_CLOSED, 'Exit'):
+            break
+        elif event == 'Submit':
+            user_input = values['-IN-']
+            '''User input processing'''
+            if 'north' in user_input.lower():
+                output = game_play('North')
+            elif 'south' in user_input.lower():
+                output = game_play('South')
+            elif 'fight' in user_input.lower():
+                output = game_play('Fight')
+            else:
+                output = 'Unknown command!'
+
+            '''Update the game display'''
+            window['-OUTPUT-'].update(output)
+            window['-IMG-'].update(game_places[game_state]['Image'])
+
+    window.close()
+
